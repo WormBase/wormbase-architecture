310e7847ec6e988a2fa78ff1baf6078c870f2ebb
Sibyl <sibyl@wormbase.org>
2018-09-14 19:39:28 +0000

    tidy up acedb Dockerfile and entrypoint

--- roles/acedb/files/startserver.sh
+++ roles/acedb/files/startserver.sh
@@ -1,3 +1,3 @@
 docker stop acedb;
 docker rm acedb;
-docker run -d --name acedb --restart unless-stopped --net wb-network -v /usr/local/wormbase/acedb/wormbase:/acedb/wormbase/ --publish 2005:2005 -t wormbase/acedb /root/acedb/acedb-4.9.52/sgifaceserver /acedb/wormbase 2005
+docker run -d --name acedb --restart unless-stopped --net wb-network -v /usr/local/wormbase/acedb/wormbase:/root/acedb/wormbase/ --publish 2005:2005 -t wormbase/acedb
